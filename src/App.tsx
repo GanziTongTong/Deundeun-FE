57d1e240d65e9043daa05e2f8ed390a6cb7d8220
Neogul02 <wlsgud2414@kangnam.ac.kr>
2025-11-11 21:34:15 +0900

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold text-blue-300 '>vite + react + tailwind + zustand basic environment</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
